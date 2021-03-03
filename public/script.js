c64e49aa5faf33b56765d0f22657f501c7285af1
Avaldesuso <78240612+Avaldesuso@users.noreply.github.com>
2021-03-02 21:51:43 -0500

    Initial code

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
+
+const restaurants = [];
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => restaurants.push(...data))
+
+function findMatches(wordToMatch, restaurants) {
+    return restaurants.filter(name => {
+
+
+        const regex = new RegExp(wordToMatch, 'gi');
+        return name.city.match(regex) || AnimationPlaybackEvent.state.match(regex)
+    })
+}
