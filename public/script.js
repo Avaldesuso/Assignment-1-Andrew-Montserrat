b4f7d841c20a612291c73b5ea8fc531328e9205c
Avaldesuso <78240612+Avaldesuso@users.noreply.github.com>
2021-03-04 21:53:20 -0500

    here we go again

--- public/script.js
+++ public/script.js
@@ -1,3 +1,42 @@
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
+const cities = [];
+
+fetch(endpoint)
+.then(blob => blob.json())
+.then(data => cities.push(...data))
+
+function findMatches(wordToMatch, cities){
+    return cities.filter(place => { 
+        const regex = new RegExp(wordToMatch,'gi');
+        return place.name.match(regex) || place.category.match(regex)
+    })};
+
+function displayMatches() {
+    const matchArray = findMatches(this.value,cities);
+    let html = matchArray.map(place => {
+        const regex = new RegExp(this.value,'gi');
+        return `
+        <li>
+        <span class="name">${place.name}  </span>
+        <span class="category">${place.category} || </span>
+        <span class="city">${place.city}</span>
+        </li>
+        `;
+    }).join('');
+
+    if (this.value.length == 0) {
+        html = [];}
+
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.text'); 
+const suggestions = document.querySelector('.suggestions');
+
+
+
+/*
 import { json } from "express";
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
@@ -41,3 +80,4 @@ function displayMatches(){
   
   searchInput.addEventListener('change', displayMatches);
   searchInput.addEventListener('keyup', displayMatches);
+*/
